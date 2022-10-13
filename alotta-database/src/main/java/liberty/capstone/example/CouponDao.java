77a1a558bbf43030bbe675f5aeb7d07d57f28a4c
James <45608681+jpooch98@users.noreply.github.com>
2022-10-13 16:53:42 -0400

    More Progress
    Getting errors in the Test

--- alotta-database/src/main/java/liberty/capstone/example/CouponDao.java
+++ alotta-database/src/main/java/liberty/capstone/example/CouponDao.java
@@ -3,4 +3,5 @@ package liberty.capstone.example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponDao extends JpaRepository<Coupon, Long> {
+    
 }
--- alotta-database/src/test/java/liberty/capstone/example/CouponDaoTest.java
+++ alotta-database/src/test/java/liberty/capstone/example/CouponDaoTest.java
@@ -8,18 +8,26 @@ import org.springframework.test.context.jdbc.Sql;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
-@Sql(scripts = "/.sql")
-class CouponDaoTest {
+@Sql(scripts = "/coupon.sql")
+class CouponTest {
     @Autowired
     private CouponDao couponDao;
 
     @Test
     void saveExample() {
-        final ExampleEntity entity = new ExampleEntity();
-        entity.setName("Example One");
+        final Coupon entity = new Coupon();
+        entity.setCouponType("This is a test of the Type field");
+        entity.setPercentageOff((long) 74);
+        entity.setDollarsOff((long)10.25);
+        entity.setCouponInfo("This is a test of the info field");
+        entity.setFoodCategories("American");
+        entity.setNumberOfCouponsPerCustomer((long)11);
+        entity.setTotalNumberOfCoupons((long)12);
+        entity.setNumberOfCouponsSold((long)11.0);
+    
         couponDao.save(entity);
 
-        assertTrue(couponDao.findById(entity.getId()).isPresent());
+        assertTrue(CouponDao.findById(entity.getId()).isPresent());
     }
 
 }

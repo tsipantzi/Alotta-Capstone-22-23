5581eaafd1d2cb379dc9f3feb2c0f8f997eadab4
Ben <blhester@liberty.edu>
2022-10-14 10:43:20 -0400

    Ben-Restaurant  + Created the restaurant-setup.sql, RestaurantDao, RestaurantEntity, and RestaurantDaoTest

---
+++ alotta-database/src/test/java/liberty/capstone/restaurant/RestaurantDaoTest.java
@@ -0,0 +1,26 @@
+package liberty.capstone.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@Sql(scripts = "/restaurant-setup.sql")
+class RestaurantDaoTest {
+    @Autowired
+    private RestaurantDao restaurantDao;
+
+    @Test
+    void saveAndFindByName() {
+        final RestaurantEntity entity = new RestaurantEntity();
+        entity.setName("Test Restaurant Name");
+        entity.setPhoneNumber("123-456-7890");
+
+        var entityToSave = restaurantDao.findByName(entity.getName()).orElse(entity);
+        restaurantDao.save(entityToSave);
+        assertTrue(restaurantDao.findByName(entity.getName()).isPresent());
+    }
+}

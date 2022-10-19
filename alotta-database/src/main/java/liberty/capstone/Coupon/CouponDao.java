1af98a1846c61de0c40e6edd3dc7c8bf102fe695
James <45608681+jpooch98@users.noreply.github.com>
2022-10-19 17:09:29 -0400

    Fixed some incorrect files

--- alotta-database/src/main/java/liberty/capstone/Coupon/CouponDao.java
+++
@@ -1,7 +0,0 @@
-package liberty.capstone.Coupon;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CouponDao extends JpaRepository<Coupon, Long> {
-    
-}
--- alotta-database/src/main/java/liberty/capstone/example/ExampleDao.java
+++
@@ -1,6 +0,0 @@
-package liberty.capstone.example;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExampleDao extends JpaRepository<ExampleEntity, Long> {
-}

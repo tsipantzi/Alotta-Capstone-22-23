284b88007eb5cd3a5a763b048e15c33940e1dc71
James <45608681+jpooch98@users.noreply.github.com>
2022-10-27 11:29:01 -0400

    ok

---
+++ alotta-process/src/main/java/liberty/capstone/process/coupon/CouponController.java
@@ -0,0 +1,8 @@
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/restaurant/{restauntId}/coupon")
+public class CouponController {
+    private final CouponService couponService;
+    
+}

fcc17a7c3461c789104ee9e9492f54fb547d3961
Dean Natale <97637351+danatale16@users.noreply.github.com>
2022-12-06 04:17:18 -0500

    first attempt at front-end draft 1

---
+++ alotta_client/lib/assets/colors/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const primaryCream = Color(0xfff8f4eb);
+const primaryOrangeSwatch = {
+  50: Color.fromRGBO(235, 116, 80, .1),
+  100: Color.fromRGBO(235, 116, 80, .2),
+  200: Color.fromRGBO(235, 116, 80, .3),
+  300: Color.fromRGBO(235, 116, 80, .4),
+  400: Color.fromRGBO(235, 116, 80, .5),
+  500: Color.fromRGBO(235, 116, 80, .6),
+  600: Color.fromRGBO(235, 116, 80, .7),
+  700: Color.fromRGBO(235, 116, 80, .8),
+  800: Color.fromRGBO(235, 116, 80, .9),
+  900: Color.fromRGBO(235, 116, 80, 1),
+};
+MaterialColor primaryOrangeMaterialColor =
+    const MaterialColor(0xffeb7450, primaryOrangeSwatch);
+
+const primaryGreen = Color(0xff4d9f6b);

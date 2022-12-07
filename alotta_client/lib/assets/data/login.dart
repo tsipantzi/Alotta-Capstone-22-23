8f35e71e5793779577774dd23341a9b73c3802f5
James <45608681+jpooch98@users.noreply.github.com>
2022-12-06 20:53:03 -0500

    Create login.dart

---
+++ alotta_client/lib/assets/data/login.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
+
+class User {
+    final String username;
+    final String password;
+
+
+  User({
+    required this.username,
+    required this.password
+}

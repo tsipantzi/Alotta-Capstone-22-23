f7f160194ba127910c54f50c24bc510ed6b484eb
James <45608681+jpooch98@users.noreply.github.com>
2023-02-08 11:12:11 -0500

    Update UserRestaurantController.java

--- alotta-process/src/main/java/liberty/capstone/process/userrestaurant/UserRestaurantController.java
+++ alotta-process/src/main/java/liberty/capstone/process/userrestaurant/UserRestaurantController.java
@@ -1,51 +1,51 @@
 package liberty.capstone.process.userrestaurant;
 
+import liberty.capstone.core.restaurant.Restaurant;
+import liberty.capstone.core.userrestaurant.UserRestaurant;
+import liberty.capstone.core.userrestaurant.UserRestaurantService;
+import liberty.capstone.core.appuser.AppUser;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import liberty.capstone.core.appuser.AppUser;
-import liberty.capstone.core.restaurant.Restaurant;
-import liberty.capstone.core.userrestaurant.UserRestaurant;
-import liberty.capstone.core.userrestaurant.UserRestaurantS
-
 
 
 @Slf4j
-@R estController
+@RestController
 @RequiredArgsConstructor
 //not sure if appUser is correct here
 @RequestMapping("/user/restaurants")
 
 public class UserRestaurantController {
-    pr ivate final UserRestaurantService userRestaurantService;
+    private final UserRestaurantService userRestaurantService;
 
-    // List<UserRestaurant> getRestaurantsByUsername(String username);
+    //List<UserRestaurant> getRestaurantsByUsername(String username);
 
-    //UserRestaurant saveRestaurant(UserRestaurant userRestaurant); 
+    //UserRestaurant saveRestaurant(UserRestaurant userRestaurant);
     @GetMapping
-                ist<Restaurant> getRestaurantsForUser(@RequestParam final String userName){
-     
-
+    public List<Restaurant> getRestaurantsForUser(@RequestParam final String userName){
+        return userRestaurantService.getRestaurantsByUsername(userName).stream()
+            .map(UserRestaurant::getRestaurant).collect(Collectors.toList());
     }
     
-            PostMapping 
+    @PostMapping
     public Restaurant saveRestaurantForUser(@RequestParam final String userName,
      @RequestBody final Restaurant restaurant){
         final AppUser appUser = new AppUser();
         appUser.setUsername(userName);
 
-        final UserRestaurant userRestaurant = new UserRestaurant(appUser, rest
-                
-     
+        final UserRestaurant userRestaurant = new UserRestaurant(appUser, restaurant);
 
+        return Stream.of(userRestaurantService.saveRestaurant(userRestaurant))
+        .map(UserRestaurant::getRestaurant).collect(Collectors.toList()).get(0);
+    }
     
     
 }

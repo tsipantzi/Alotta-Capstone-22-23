d91aa8953bb10cf8b6c736e539cf3b5d2340594b
Dean Natale <97637351+danatale16@users.noreply.github.com>
2023-02-27 11:09:35 -0500

    Added some simple formatting to the resturant object text

--- alotta_client/lib/pages/coupon_manager_page.dart
+++ alotta_client/lib/pages/coupon_manager_page.dart
@@ -28,17 +28,29 @@ class CouponManagerPage extends StatelessWidget {
                 ),
               ),
             ),
-            Text(currentRestaurant.name),
-            Text(currentRestaurant.phoneNumber),
-            Text(currentRestaurant.email),
-            Text(currentRestaurant.aboutMe),
-            Text("Zip Code: ${currentRestaurant.zipCode}"),
-            Text("Categoies: ${currentRestaurant.foodCategories}"),
-            Text(
-                "Max Catering Size: ${currentRestaurant.maxCateringSizePerPerson}"),
-            Text("Minimum Notice: ${currentRestaurant.minimumNotice}"),
-            Text(
-                "Number of Active Coupons: ${currentRestaurant.numberOfActiveCoupons}"),
+            Container(
+                padding: const EdgeInsets.all(15),
+                color: Colors.white,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(currentRestaurant.name,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    Text(currentRestaurant.phoneNumber),
+                    Text(currentRestaurant.email),
+                    Text("Zip Code: ${currentRestaurant.zipCode}"),
+                    Text("Categories: ${currentRestaurant.foodCategories}"),
+                    Text(
+                        "Max Catering Size: ${currentRestaurant.maxCateringSizePerPerson}"),
+                    Text("Minimum Notice: ${currentRestaurant.minimumNotice}"),
+                    Text(
+                        "Number of Active Coupons: ${currentRestaurant.numberOfActiveCoupons}"),
+                    Text(currentRestaurant.aboutMe),
+                  ],
+                ))
           ],
         ),
       ),

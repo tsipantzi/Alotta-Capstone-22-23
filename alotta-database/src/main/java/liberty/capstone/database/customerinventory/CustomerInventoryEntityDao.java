e22588d50316c027aeb4a0b9b51b341d337b9a6c
Dean Natale <97637351+danatale16@users.noreply.github.com>
2023-03-28 10:22:40 -0400

    Changed method name in Dao

--- alotta-database/src/main/java/liberty/capstone/database/customerinventory/CustomerInventoryEntityDao.java
+++ alotta-database/src/main/java/liberty/capstone/database/customerinventory/CustomerInventoryEntityDao.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+@SuppressWarnings({"MethodName", "PMD.MethodNamingConventions"})
 public interface CustomerInventoryEntityDao extends JpaRepository<CustomerInventoryEntity, Long> {
-    List<CustomerInventoryEntity> findAllByCustomer_Id(Long customerId);
+    List<CustomerInventoryEntity> findAllByUser_Id(Long customerId);
 }

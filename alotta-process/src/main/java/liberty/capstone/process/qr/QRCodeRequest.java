4fc6bb7e4f42b3ae475828839efadcbe199d16b7
Ben <blhester@liberty.edu>
2023-04-07 21:49:43 -0400

    Fix checkstyle errors

--- alotta-process/src/main/java/liberty/capstone/process/qr/QRCodeRequest.java
+++ alotta-process/src/main/java/liberty/capstone/process/qr/QRCodeRequest.java
@@ -5,19 +5,21 @@ import lombok.Data;
 
 @Data
 @JsonSerialize
+@SuppressWarnings({"MemberName", "MultipleStringLiterals", "PMD.FieldNamingConventions", "ConstantName",
+        "PMD.AvoidDuplicateLiterals"})
 public class QRCodeRequest {
-    private final String frame_name = "no-frame";
-    private final String image_format = "PNG";
-    private final String background_color = "#ffffff";
-    private final String foreground_color = "#eb7450";
-    private final String marker_right_inner_color = "#4d9f6b";
-    private final String marker_right_outer_color = "#4d9f6b";
-    private final String marker_left_inner_color = "#4d9f6b";
-    private final String marker_left_outer_color = "#4d9f6b";
-    private final String marker_bottom_inner_color = "#4d9f6b";
-    private final String marker_bottom_outer_color = "#4d9f6b";
-    private final String marker_left_template = "version13";
-    private final String marker_right_template = "version13";
-    private final String marker_bottom_template = "version13";
+    private static final String frame_name = "no-frame";
+    private static final String image_format = "PNG";
+    private static final String background_color = "#ffffff";
+    private static final String foreground_color = "#eb7450";
+    private static final String marker_right_inner_color = "#4d9f6b";
+    private static final String marker_right_outer_color = "#4d9f6b";
+    private static final String marker_left_inner_color = "#4d9f6b";
+    private static final String marker_left_outer_color = "#4d9f6b";
+    private static final String marker_bottom_inner_color = "#4d9f6b";
+    private static final String marker_bottom_outer_color = "#4d9f6b";
+    private static final String marker_left_template = "version13";
+    private static final String marker_right_template = "version13";
+    private static final String marker_bottom_template = "version13";
     private String qr_code_text;
 }

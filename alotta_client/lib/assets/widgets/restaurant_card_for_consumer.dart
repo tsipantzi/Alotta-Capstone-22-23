1871c9dd1461ae312393d34b47db378b264287bd
James <45608681+jpooch98@users.noreply.github.com>
2023-04-25 16:02:39 -0400

    Create restaurant_card_for_consumer.dart

---
+++ alotta_client/lib/assets/widgets/restaurant_card_for_consumer.dart
@@ -0,0 +1,63 @@
+import 'package:alotta_client/assets/data/restaurant.dart';
+import 'package:flutter/material.dart';
+
+import '../../main.dart';
+import '../colors/colors.dart';
+import '../data/user_restaurant.dart';
+
+class RestaurantCardForConsumer extends StatelessWidget {
+  final Restaurant restaurant;
+
+  const RestaurantCardForConsumer({
+    super.key,
+    required this.restaurant,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 250,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Flexible(
+              child: Container(
+            margin: const EdgeInsets.fromLTRB(10, 0, 5, 0),
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.transparent,
+              image: DecorationImage(
+                image: AssetImage('images/restaurant_placeholder.png'),
+              ),
+            ),
+          )),
+          Text(
+            "Zip Code: ${restaurant.zipCode}",
+            style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
+          ),
+          Text(
+            "Categories: ${restaurant.foodCategories}",
+            style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
+          ),
+          Text(
+            "Max Catering Size: ${restaurant.maxCateringSizePerPerson}",
+            style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
+          ),
+          Text(
+            "Minimum Notice: ${restaurant.minimumNotice}",
+            style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
+          ),
+          Text(
+            "Number of Active Coupons: ${restaurant.numberOfActiveCoupons}",
+            style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
+          ),
+          Text(
+            restaurant.aboutMe,
+            style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
+          ),
+        ],
+      ),
+    );
+  }
+}

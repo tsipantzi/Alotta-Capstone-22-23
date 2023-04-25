d55dc7463f571f299ec943f3628c3abb66f37be9
James <45608681+jpooch98@users.noreply.github.com>
2023-04-25 16:19:34 -0400

    Update restaurant_card_for_consumer.dart

--- alotta_client/lib/assets/widgets/restaurant_card_for_consumer.dart
+++ alotta_client/lib/assets/widgets/restaurant_card_for_consumer.dart
@@ -15,49 +15,44 @@ class RestaurantCardForConsumer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 250,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Flexible(
-              child: Container(
-            margin: const EdgeInsets.fromLTRB(10, 0, 5, 0),
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.transparent,
-              image: DecorationImage(
-                image: AssetImage('images/restaurant_placeholder.png'),
-              ),
+    return Stack(
+      children: <Widget>[
+        Flexible(
+            child: Container(
+          margin: const EdgeInsets.fromLTRB(10, 0, 5, 0),
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.transparent,
+            image: DecorationImage(
+              image: AssetImage('images/restaurant_placeholder.png'),
             ),
-          )),
-          Text(
-            "Zip Code: ${restaurant.zipCode}",
-            style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
           ),
-          Text(
-            "Categories: ${restaurant.foodCategories}",
-            style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
-          ),
-          Text(
-            "Max Catering Size: ${restaurant.maxCateringSizePerPerson}",
-            style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
-          ),
-          Text(
-            "Minimum Notice: ${restaurant.minimumNotice}",
-            style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
-          ),
-          Text(
-            "Number of Active Coupons: ${restaurant.numberOfActiveCoupons}",
-            style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
-          ),
-          Text(
-            restaurant.aboutMe,
-            style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
-          ),
-        ],
-      ),
+        )),
+        Text(
+          "Zip Code: ${restaurant.zipCode}",
+          style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
+        ),
+        Text(
+          "Categories: ${restaurant.foodCategories}",
+          style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
+        ),
+        Text(
+          "Max Catering Size: ${restaurant.maxCateringSizePerPerson}",
+          style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
+        ),
+        Text(
+          "Minimum Notice: ${restaurant.minimumNotice}",
+          style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
+        ),
+        Text(
+          "Number of Active Coupons: ${restaurant.numberOfActiveCoupons}",
+          style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
+        ),
+        Text(
+          restaurant.aboutMe,
+          style: MyApp.platformBodyStyle.copyWith(color: primaryCream),
+        ),
+      ],
     );
   }
 }
